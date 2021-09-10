3c2bbbe471c261d3ab762413b321f8aaa66e2322
gaballench <gaballench@gmail.com>
2021-09-10 17:35:08 -0300

    Added tests for findParams to find pars for the normal and poisson distributions

--- tests/testthat/test-findParams.R
+++ tests/testthat/test-findParams.R
@@ -1,3 +1,6 @@
+# set seed
+set.seed(1453)
+
 # check input quantiles and percentiles vectors of the same length
 test_that("error if length of vectors p and q are not the same", {
     expect_error(findParams(q = c(-1, 0, 1), p = c(0.25, 0.75)), "length of p and q must be equal", ignore.case = TRUE)
@@ -13,4 +16,43 @@ test_that("omitting the initVal parameter throws a warning", {
                    "initVals not provided", ignore.case = TRUE)
 })
 
+# check that find params works with the normal distribution
+r_mean <- runif(n = 1, min = -4, max = 4)
+r_sd <- runif(n = 1, min = 1, max = 10)
+r_q <- c(0.025, 0.5, 0.975)
+r_p <- pnorm(q = r_q, mean = r_mean, sd = r_sd)
 
+test_that("findParams finds an arbitrary mean for the normal distribution", {
+    expect_equal(findParams(q = r_q,
+                            p = r_p,
+                            output = "complete",
+                            pdfunction = "pnorm",
+                            params = c("mean", "sd"),
+                            initVals = c(r_mean, r_sd))$par[1],
+                 r_mean)
+})
+
+test_that("findParams finds an arbitrary sd for the normal distribution", {
+    expect_equal(findParams(q = r_q,
+                            p = r_p,
+                            output = "complete",
+                            pdfunction = "pnorm",
+                            params = c("mean", "sd"),
+                            initVals = c(r_mean, r_sd))$par[2],
+                 r_sd)
+})
+
+# check that find params works with the poisson distribution
+r_lambda <- runif(n = 1, min = 0, max = 5)
+r_q <- c(0.025, 0.5, 0.975)
+r_p <- ppois(q = r_q, lambda = r_lambda)
+
+test_that("findParams finds an arbitrary lambda for the poisson  distribution", {
+    expect_equal(findParams(q = r_q,
+                            p = r_p,
+                            output = "complete",
+                            pdfunction = "ppois",
+                            params = c("lambda"),
+                            initVals = c(r_lambda))$par[1],
+                 r_lambda)
+})
