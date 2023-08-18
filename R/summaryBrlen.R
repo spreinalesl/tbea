be3f8b5b9214ed3a550c6ad79d0042b801a19a06
gaballench <gaballench@gmail.com>
2023-08-18 11:58:38 +0100

    There was a bug when picking the outgroup for summarisation of trees, such tip was hard-coded as t1 and therefore the bug went silent when using simulated trees from ape. However on trees with tip labels other than tN, it failed as t1 was not found to be a tip. Raising patch number.

--- R/summaryBrlen.R
+++ R/summaryBrlen.R
@@ -36,10 +36,12 @@ summaryBrlen <- function(mphy, method) {
     if (inherits(mphy, "multiPhylo")) {
         mphy <- list(mphy)
     }
+    # pick an arbitrary branch for rooting
+    outgroup <- mphy[[1]][[1]]$tip.label[1]
     summary_trees <- vector("list", length=length(mphy))
     iter <- 1
     for (i in mphy) {
-        rooted_tr <- root(i, outgroup="t1", resolve.root=TRUE)
+        rooted_tr <- root(i, outgroup=outgroup, resolve.root=TRUE)
         for (k in seq_along(rooted_tr)) {
             rooted_tr[[k]] <- makeNodeLabel(rooted_tr[[k]], method="md5sum")
         }
