23019f7f086d98ebb1d6c25413f8fcc76d877861
gaballench <gaballench@gmail.com>
2024-05-24 07:56:04 -0300

    Using globalVariables in order to skip the note on x as undefined global variable in conflate. This should close all notes raised by R CMD check on cran

---
+++ R/tbea-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# set globalVariable for x in conflate
+if(getRversion() >= "3.5.0")  utils::globalVariables(c("x"))
